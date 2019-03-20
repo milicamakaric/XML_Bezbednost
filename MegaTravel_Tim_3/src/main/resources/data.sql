9e0bdc62fe2768e2265f296c011bb6a4b30dbb54
Milica Makaric <makaric.milica@uns.ac.rs>
2019-03-20 18:15:27 +0100

    model

---
+++ MegaTravel_Tim_3/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into software (id, name) values (1, 'soft1');
+insert into software (id, name) values (2, 'soft2');
+insert into software (id, name) values (3, 'soft3');
+insert into software (id, name) values (4, 'soft4');
+insert into software (id, name) values (5, 'soft5');
