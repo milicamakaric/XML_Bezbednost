f8ec9dffb6d839734c45a36546fb84ea47071da3
marinagavric <marina.gavric@uns.ac.rs>
2019-03-21 11:36:17 +0100

    Povlacenje sertifikata

--- MegaTravel_Tim_3/src/main/resources/static/js/software.js
+++ MegaTravel_Tim_3/src/main/resources/static/js/software.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+	$("#revokeDiv").hide();
+	
 	$.ajax({
         type: 'GET',
         url: '/api/softwares/getNotCertificated',
@@ -35,14 +37,14 @@ $(document).ready(function()
 
 function insertWithCertificate(software)
 {
-	var tr = $('<tr><td>' + software.name +  '</td><td></td></tr>');
+	var tr = $('<tr><td>' + software.name +  '</td><td><button id="revoke'+software.id+'" class=\"btn btn-default	btn-md\" onclick=revokeCertificate('+software.id+')>Revoke</button></td></tr>');
 	
 	$('#lista').append(tr);
 }
 
 function insertWithoutCertificate(software)
 {
-	var tr = $('<tr><td>' + software.name +  '</td><td><button id="add'+software.id+'" onclick=addCertificate('+software.id+')>Add certicicate</button></td></tr>');
+	var tr = $('<tr><td>' + software.name +  '</td><td><button id="add'+software.id+'" class=\"btn btn-default	btn-md\" onclick=addCertificate('+software.id+')>Add certificate</button></td></tr>');
 	
 	$('#lista').append(tr);
 }
@@ -50,4 +52,30 @@ function insertWithoutCertificate(software)
 function addCertificate(id)
 {
 	window.location.href="certificate.html?id=" + id;
+}
+function revokeCertificate(id)
+{
+	$("#btnRevoke").empty();
+	$("#btnRevoke").append('<button type=\"button\" id=\"confirmReason\"onclick=revocation('+id+') class=\"btn btn-default btn-md\">Confirm</button>');
+	
+	$("#revokeDiv").show();
+}
+function revocation(id)
+{
+	$("#btnRevoke").empty();
+	$("#revokeDiv").hide();
+
+	var reason=$("#reason").val();
+
+	$.ajax({
+		type : 'POST',
+		url : "/api/certificates/revoke/"+id,
+		success : function(data) {
+				console.log('revoked');
+			
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			alert('greska');
+		}
+	});
 }
