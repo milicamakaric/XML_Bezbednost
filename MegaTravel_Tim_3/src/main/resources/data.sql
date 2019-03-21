e4bee4b262916d3953563fd1c45178832aed8724
Milica Makaric <makaric.milica@uns.ac.rs>
2019-03-21 10:59:30 +0100

    dodat softwares.html

--- MegaTravel_Tim_3/src/main/resources/data.sql
+++ MegaTravel_Tim_3/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 delete from software;
 
 insert into software (id, name, certificated) values (1, 'soft1', false);
-insert into software (id, name, certificated) values (2, 'soft2', false);
+insert into software (id, name, certificated) values (2, 'soft2', true);
 insert into software (id, name, certificated) values (3, 'soft3', false);
-insert into software (id, name, certificated) values (4, 'soft4', false);
+insert into software (id, name, certificated) values (4, 'soft4', true);
 insert into software (id, name, certificated) values (5, 'soft5', false);
