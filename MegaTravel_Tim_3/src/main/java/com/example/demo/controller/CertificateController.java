f5b5fe1ca8a0a63f6dcca80e58533a71df4b7260
Milica Makaric <makaric.milica@uns.ac.rs>
2019-03-22 16:01:58 +0100

    sitne prepravke

--- MegaTravel_Tim_3/src/main/java/com/example/demo/controller/CertificateController.java
+++ MegaTravel_Tim_3/src/main/java/com/example/demo/controller/CertificateController.java
@@ -115,36 +115,7 @@ public class CertificateController {
 		
 		return certificate;
 	}
-/*
-	private java.security.cert.Certificate createCertificateWithGen(Long id_cert, SubjectData subjectData, IssuerData issuerData,
-			PublicKey publicKey, Date start_date_cert, Date end_date_cert) {
-		
-		try {
-			
-		    
-			//Generise se sertifikat za subjekta, potpisan od strane issuer-a
-			CertificateGenerator cg = new CertificateGenerator();
-			X509Certificate cert = cg.generateCertificate(subjectData, issuerData);
-			
-			//Moguce je proveriti da li je digitalan potpis sertifikata ispravan, upotrebom javnog kljuca izdavaoca
-			cert.verify(publicKey);
-			return cert;
-		} catch(CertificateException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (NoSuchProviderException e) {
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			System.out.println("\nValidacija neuspesna :(");
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
-	*/
+	
 	private IssuerData generateIssuerData(PrivateKey private1, User issuer) {
 		X500NameBuilder builder = new X500NameBuilder(BCStyle.INSTANCE);
 	    
