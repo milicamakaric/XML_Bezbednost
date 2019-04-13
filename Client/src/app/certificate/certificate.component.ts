0c53c378aaae2e6b224d656072ada0b806f73c2c
marinagavric <marina.gavric@uns.ac.rs>
2019-04-13 16:08:36 +0200

    LogOut

--- Client/src/app/certificate/certificate.component.ts
+++ Client/src/app/certificate/certificate.component.ts
@@ -23,46 +23,40 @@ export class CertificateComponent implements OnInit {
   constructor(private certificateService: CertificateServiceService, private userService: UserServiceService, private route: ActivatedRoute) { 
     this.route.params.subscribe( params => {this.self = params.self, this.id=params.id});
     this.today = new Date();
-    this.certificated_users=null;
+    this.certificated_users = null;
   }
 
   ngOnInit() {
     console.log('parametar: ' + this.self);
 
-    if(this.self == "nonself")
-    {
+    if (this.self == 'nonself'){
      this.certificated_users =  this.userService.getCertificatedUsers();
     // this.userService.getCertificatedUsers().subscribe(data => this.certificated_users = data);
-      if(this.certificated_users==null)
-      {
-        window.alert("There are not certificated issuers!");
-        window.location.href="http://localhost:4200/softwares";
-      }
-      else
-      {
-        document.getElementById("skriveno").removeAttribute("hidden");
+     if (this.certificated_users == null) {
+        window.alert('There are not certificated issuers!');
+        window.location.href='http://localhost:4200/softwares';
+    } else {
+        document.getElementById('skriveno').removeAttribute('hidden');
 
-        for(let u of this.certificated_users)
-       {
+        for ( let u of this.certificated_users){
          let korisnik = u as User;
-        let opt =  document.createElement("option");
-        opt.innerHTML= korisnik.name + " " + korisnik.surname;
-       document.getElementById("lista_issuer").appendChild(opt);
+         let opt =  document.createElement('option');
+         opt.innerHTML = korisnik.name + ' ' + korisnik.surname;
+         document.getElementById('lista_issuer').appendChild(opt);
        }
       }
     }
-    
   }
 
-  createCertificate(){
-    if(this.self == 'self'){
+  createCertificate() {
+    if (this.self == 'self') {
       console.log('id:' + this.id);
       console.log('start:' + this.startDate);
-      console.log('end:' + this.endDate);  
+      console.log('end:' + this.endDate);
       this.certificateService.createSelfCertificate(this.id as string, this.startDate, this.endDate).subscribe(
         data => window.location.href = 'http://localhost:4200/softwares'
         );
-    }else{
+    } else {
       console.log('id:' + this.id);
       console.log('start:' + this.startDate);
       console.log('end:' + this.endDate);  
@@ -71,7 +65,6 @@ export class CertificateComponent implements OnInit {
   }
 
   startDateChanged(){
-    
   }
 
 }
