d309d43e960db390dfedb04ae4b633dc9d8e5ed4
Milica Makaric <makaric.milica@uns.ac.rs>
2019-04-16 23:40:53 +0200

    https communication

--- MegaTravel_Tim_3/src/main/resources/data.sql
+++ MegaTravel_Tim_3/src/main/resources/data.sql
@@ -21,9 +21,6 @@ insert into users_roles (user_id, role_id) values (1,1);
 
 insert into certificate (id, ca, end_date, id_issuer, id_subject, reason_for_revokation, revoked, start_date) values (57, 1, '2021-04-15 22:00:00.000000', 1, 1, '', 0, '2019-04-15 22:00:00.000000');
 
-insert into user (id, name, surname, email, password, certificated) values (1, 'admin', 'admin', 'admin@gmail.com', '$2a$10$Z64.qrnsDUmTQvUrXQ14hu8AnXR6ZjquesE8oqW/izeiUYMAA8wDe', false);
-insert into authority (id, name) values (1, 'ROLE_ADMIN');
-insert into user_authority (user_id, authority_id) values (1, 1);
 
 insert into software (id, name, certificated, alias) values (1, 'soft1', false, 'localSoft1');
 insert into software (id, name, certificated, alias) values (2, 'soft2', false, 'localSoft2');
--- agent/src/main/java/com/example/agent/controller/AgentController.java
+++ agent/src/main/java/com/example/agent/controller/AgentController.java
@@ -1,21 +1,54 @@
 package com.example.agent.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
+import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 
 @RestController
 @RequestMapping(value="api/agent")
 @CrossOrigin(origins = "http://localhost:4201")
 public class AgentController {
 	
+	private KeyStore keyStore;
+	private String certPath;
+	private String keyStorePass;
+	private String uri;
+	
+	@PostConstruct
+	public void init(){
+		certPath = "C:\\Users\\Makaric\\Desktop\\globalKeyStore.p12";
+		uri = "https://localhost:8443/api/users/communication";
+		keyStorePass = "certificatePass1";
+		try {
+			keyStore = KeyStore.getInstance("PKCS12");
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
 	@RequestMapping(
 			value = "/communicate",
 			method = RequestMethod.POST,
@@ -27,7 +60,15 @@ public class AgentController {
 		RequestEntity<Object> requestEntity = null;
 		
         RestTemplate template = new RestTemplate();
-        return template.exchange("https://localhost:8443/api/users/communication", HttpMethod.GET, requestEntity, String.class);
+        File file = new File(certPath);
+        InputStream is = new FileInputStream(file);
+        keyStore.load(is, keyStorePass.toCharArray());
+        
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(file, keyStorePass.toCharArray()).loadKeyMaterial(keyStore, keyStorePass.toCharArray()).build();
+		HttpClient httpClient = HttpClients.custom().setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER).setSSLContext(sslContext).build();
+        template.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+        
+        return template.exchange(uri, HttpMethod.GET, requestEntity, String.class);
 	}
 
 }
