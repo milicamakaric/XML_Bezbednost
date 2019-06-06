805694c6a41a459114d2b1cc138a0d652aeea953
Milica Makaric <makaric.milica@uns.ac.rs>
2019-06-06 08:29:27 +0200

    model

---
+++ MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/MegaTravelXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.MegaTravel_XML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTravelXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTravelXmlApplication.class, args);
+	}
+
+}
---
+++ MegaTravel_XML/src/test/java/com/example/MegaTravel_XML/MegaTravelXmlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.MegaTravel_XML;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MegaTravelXmlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
