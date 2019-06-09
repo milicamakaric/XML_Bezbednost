377d1099fe4b5164556b6c33cef8f5667edf9c24
AndrijanaJeremic <44624036+AndrijanaJeremic@users.noreply.github.com>
2019-06-09 09:18:39 +0200

    izmene

--- MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/controller/AccommodationController.java
+++ MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/controller/AccommodationController.java
@@ -1,9 +1,16 @@
 package com.example.MegaTravel_XML.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.MegaTravel_XML.model.Accommodation;
 import com.example.MegaTravel_XML.services.AccommodationServiceImpl;
 
 @RestController
@@ -12,4 +19,14 @@ public class AccommodationController {
 
 	@Autowired
 	private AccommodationServiceImpl accommodationService;
+	
+	@RequestMapping(value="/getAll", 
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	public ResponseEntity<List<Accommodation>> getAllAccommodations(){		
+		System.out.println("get all acc");
+		List<Accommodation> accommodations = accommodationService.getAll();
+		return new ResponseEntity<List<Accommodation>>(accommodations, HttpStatus.OK);
+	}
 }
--- MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/controller/ReservationController.java
+++ MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/controller/ReservationController.java
@@ -1,14 +1,33 @@
 package com.example.MegaTravel_XML.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.MegaTravel_XML.model.Accommodation;
+import com.example.MegaTravel_XML.model.Reservation;
 import com.example.MegaTravel_XML.services.ReservationServiceImpl;
 
 @RestController
 @RequestMapping(value="api/reservation")
 public class ReservationController {
+	
  @Autowired
  private ReservationServiceImpl reservationService;
+ 
+ @RequestMapping(value="/getAll", 
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	public ResponseEntity<List<Reservation>> getAllReservations(){		
+		System.out.println("get all res");
+		List<Reservation> reservations = reservationService.getAll();
+		return new ResponseEntity<List<Reservation>>(reservations, HttpStatus.OK);
+	}
 }
