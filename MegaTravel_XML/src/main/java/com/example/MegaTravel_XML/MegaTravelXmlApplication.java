f56a54ee15002b0f51887efd2edb2383a282457b
AndrijanaJeremic <44624036+AndrijanaJeremic@users.noreply.github.com>
2019-06-15 16:24:05 +0200

    microservices

--- MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/MegaTravelXmlApplication.java
+++ MegaTravel_XML/src/main/java/com/example/MegaTravel_XML/MegaTravelXmlApplication.java
@@ -2,8 +2,10 @@ package com.example.MegaTravel_XML;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MegaTravelXmlApplication {
 
 	public static void main(String[] args) {
