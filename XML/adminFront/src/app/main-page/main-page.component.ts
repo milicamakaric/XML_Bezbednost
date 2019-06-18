81ff16aecfc5c33f635055ad7bf324eacc4962a1
rudananastasija <rudananastasija@uns.ac.rs>
2019-06-18 16:24:26 +0200

    izmjene front

--- XML/adminFront/src/app/main-page/main-page.component.ts
+++ XML/adminFront/src/app/main-page/main-page.component.ts
@@ -9,13 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
+    show: boolean;
     type: AccommodationType;
-    constructor(private accommodationService: AccommodationServiceService,private route: ActivatedRoute) { }
+    constructor(private accommodationService: AccommodationServiceService,private route: ActivatedRoute) { 
+      this.show = false;
+    }
 
   ngOnInit() {
   }
   newAccommodationType(){
+       this.show = false;
       this.accommodationService.addAccommodationType(this.type);
+  }
+
+  addType(){
+    this.show = true;
   }
 }
